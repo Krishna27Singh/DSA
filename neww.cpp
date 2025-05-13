5fefa95136f0ada5d11b0ddcb4f46a83f366028c
Krishna27Singh <krishnasingh97857@gmail.com>
2025-05-13 13:50:36 +0530

    main

---
+++ neww.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s = "abcyy";
+    int t = 2;
+    // cout<<s.size()<<endl;
+    for(int i = 0; i<t; i++){
+        string new_s = "";
+        for(int j = 0; j<s.size(); j++){
+            if(s[j]=='z'){
+                // cout<<s[j];
+                new_s += "ab";
+            }
+            else{
+                // cout<<s[j]<<endl;
+                new_s = new_s + char(s[j]+1);
+            }
+        }
+        s = new_s;
+        cout<<s<<endl;
+        cout<<new_s<<endl;
+    }
+    cout<<s.size();
+    return 0;
+}
