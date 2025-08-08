69753a41fd3552e0b21fc1e61c986b2e9f515c0c
Krishna27Singh <krishnasingh97857@gmail.com>
2025-08-08 16:53:07 +0530

    new

---
+++ day1lib.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    string test = "krishna";
+    sort(test.begin(), test.end());
+    cout<<test<<endl;
+    return 0;
+}
