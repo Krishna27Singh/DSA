a2c70cb38f5aaddc9ea542c7be218db82b12eafc
Krishna27Singh <krishnasingh97857@gmail.com>
2025-09-11 23:04:14 +0530

    n

---
+++ cp31/1100/a.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int tc; cin>>tc;
+    while(tc--){
+        int n; cin>>n;
+        string s; cin>>s;
+
+        
+    }
+
+    return 0;
+}
